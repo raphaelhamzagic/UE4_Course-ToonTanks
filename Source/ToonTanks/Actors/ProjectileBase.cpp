4ff11005dc16149ee63cc2c95d0ea382b882c304
Raphael Hamzagic de Carvalho <raphael.hamzagic@gmail.com>
2020-09-25 09:31:35 -0300

    Making projectiles work.

--- Source/ToonTanks/Actors/ProjectileBase.cpp
+++ Source/ToonTanks/Actors/ProjectileBase.cpp
@@ -4,6 +4,7 @@
 #include "ProjectileBase.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AProjectileBase::AProjectileBase()
@@ -11,6 +12,7 @@ AProjectileBase::AProjectileBase()
 	PrimaryActorTick.bCanEverTick = false;
 
 	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
+	ProjectileMesh->OnComponentHit.AddDynamic(this, &AProjectileBase::OnHit);
 	RootComponent = ProjectileMesh;
 
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement"));
@@ -23,4 +25,18 @@ AProjectileBase::AProjectileBase()
 void AProjectileBase::BeginPlay()
 {
 	Super::BeginPlay();	
+}
+
+void AProjectileBase::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	AActor* ProjectileOwner = GetOwner();
+	if (ProjectileOwner != nullptr)
+	{
+		if (OtherActor && OtherActor != this && OtherActor != ProjectileOwner)
+		{
+			UGameplayStatics::ApplyDamage(OtherActor, Damage, ProjectileOwner->GetInstigatorController(), this, DamageType);
+		}
+
+		Destroy();
+	}
 }
--- Source/ToonTanks/Actors/ProjectileBase.h
+++ Source/ToonTanks/Actors/ProjectileBase.h
@@ -29,6 +29,9 @@ private:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Damage", meta = (AllowPrivateAccess = "true"))
 	float Damage = 50.f;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 	
 public:	
 	// Sets default values for this actor's properties
